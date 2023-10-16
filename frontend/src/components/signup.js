a2cd64429dc5d6c4f508dafa94b8e82a5452b3cd
shershon <shershon.japs@gmail.com>
2023-10-16 23:18:20 +0530

    Signup form updated

--- frontend/src/components/signup.js
+++ frontend/src/components/signup.js
@@ -1,5 +1,8 @@
 import {useState} from 'react';
 const SignUp=()=>{
+    const [fname, setFname] = useState('');
+    const [lname, setLname] = useState('');
+    const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
@@ -18,6 +21,47 @@ const SignUp=()=>{
     return (
       <div className="flex flex-col justify-center bg-lavender w-3/5 h-screen leading-8 text-grey text-center py-5 rounded">
         <h1 className="text-3xl font-bold my-5">SignUp  </h1>
+        <div className="mx-auto lg:w-3/6 w-2/5 flex flex-row justify-center">
+          <div className="w-3/6 flex flex-col justify-center mr-6">
+              <div className="flex align-start">
+                  <label className="block text-grey text-md font-bold mb-2">
+                      First Name:
+                  </label>
+              </div>
+            <input
+              className="shadow rounded py-2 px-3 text-grey mb-2"
+              type="text"
+              value={fname}
+              onChange={(e) => setFname(e.target.value)}
+            />
+          </div>
+          <div className="w-3/6 flex flex-col justify-center">
+              <div className="flex align-start">
+                  <label className="block text-grey text-md font-bold mb-2">
+                      Last Name:
+                  </label>
+              </div>
+            <input
+              className="shadow rounded py-2 px-3 text-grey mb-2"
+              type="text"
+              value={lname}
+              onChange={(e) => setLname(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className="mx-auto w-3/6 flex flex-col justify-center">
+            <div className="flex align-start">
+                <label className="block text-grey text-md font-bold mb-2">
+                    Username
+                </label>
+            </div>
+          <input
+            className="shadow rounded py-2 px-3 text-grey mb-2"
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
         <div className="mx-auto w-3/6 flex flex-col justify-center">
             <div className="flex align-start">
                 <label className="block text-grey text-md font-bold mb-2">
@@ -59,7 +103,7 @@ const SignUp=()=>{
         </div>
         <div className="w-3/6 mx-auto">
           <button
-            className="w-2/6 bg-buttonBlue text-white font-bold py-1 px-4  rounded"
+            className="w-3/6 bg-buttonBlue text-white font-bold py-1 px-2  rounded"
             type="button"
             onClick={handleLogin}
           >
