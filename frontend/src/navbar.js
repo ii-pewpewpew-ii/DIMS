80b036c9986257dc389ac3033d6e5dfc57a21f54
shershon <shershon.japs@gmail.com>
2023-11-24 21:25:11 +0530

    BG added

--- frontend/src/navbar.js
+++ frontend/src/navbar.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import logo from './assets/photo.png'
 
 const Navbar = () => {
   const navigate=useNavigate()
@@ -27,7 +28,7 @@ const Navbar = () => {
                 >
                   <img
                     className="h-8 w-8 rounded-full"
-                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                    src={logo}
                     alt=""
                   />
                 </button>
